f352e18de72e12d011c68e393496987ee5bd68a3
AMI-Kobayashi <140169096+AMI-Kobayashi@users.noreply.github.com>
2023-11-01 01:53:02 +0900

    ポートフォリオ

--- js/script.js
+++ js/script.js
@@ -1,33 +1,6 @@
 
 
 (function(d) {
-  var splash_text = $.cookie('accessdate'); //キーが入っていれば年月日を取得
-  var myD = new Date();//日付データを取得
-  var myYear = String(myD.getFullYear());//年
-  var myMonth = String(myD.getMonth() + 1);//月
-  var myDate = String(myD.getDate());//日
-      
-  if (splash_text != myYear + myMonth + myDate) {//cookieデータとアクセスした日付を比較↓
-          $("#splash").css("display", "block");//１回目はローディングを表示
-          setTimeout(function () {
-              $("#splash_logo").fadeIn(1000, function () {//1000ミリ秒（1秒）かけてロゴがフェードイン
-                  setTimeout(function () {
-              $("#splash_logo").fadeOut(1000);//1000ミリ秒（1秒）かけてロゴがフェードアウト
-                  }, 1000);//1000ミリ秒（1秒）後に処理を実行
-          setTimeout(function () {
-              $("#splash").fadeOut(1000, function () {//1000ミリ秒（1秒）かけて画面がフェードアウト
-              var myD = new Date();
-              var myYear = String(myD.getFullYear());
-              var myMonth = String(myD.getMonth() + 1);
-              var myDate = String(myD.getDate());
-              $.cookie('accessdate', myYear + myMonth + myDate); //accessdateキーで年月日を記録
-          });
-          }, 1700);//1700ミリ秒（1.7秒）後に処理を実行
-      });
-  }, 1000);//1000ミリ秒（1秒）後に処理を実行
-  }else {
-      $("#splash").css("display", "none");//同日2回目のアクセスでローディング画面非表示
-  }  
     var config = {
       kitId: 'dhm6lzq',
       scriptTimeout: 3000,
@@ -41,6 +14,17 @@
         $('.sp_btn').toggleClass('open');
         $('.sp_nav').fadeToggle();
     })
+
+
+    var splash_text = $.cookie('accessdate'); //キーが入っていれば年月日を取得
+var myD = new Date();//日付データを取得
+var myYear = String(myD.getFullYear());//年
+var myMonth = String(myD.getMonth() + 1);//月
+var myDate = String(myD.getDate());//日
+    
+if (splash_text != myYear + myMonth + myDate) {//cookieデータとアクセスした日付を比較↓
+        $("#splash").css("display", "block");//１回目はローディングを表示
+        setTimeout(function () {
 
   $(window).on('load',function(){
     $("#splash-logo").delay(1200).fadeOut('slow');//ロゴを1.2秒でフェードアウトする記述
@@ -58,6 +42,18 @@
         //この中に動かしたいJSを記載
     });
     //=====ここまで背景が伸びた後に動かしたいJSをまとめる
-        
+            var myD = new Date();
+            var myYear = String(myD.getFullYear());
+            var myMonth = String(myD.getMonth() + 1);
+            var myDate = String(myD.getDate());
+            $.cookie('accessdate', myYear + myMonth + myDate); //accessdateキーで年月日を記録
+        });
+        }, 1700);//1700ミリ秒（1.7秒）後に処理を実行
+    };
+    
+    1000;//1000ミリ秒（1秒）後に処理を実行
+else {
+    $("#splash").css("display", "none");//同日2回目のアクセスでローディング画面非表示
+}  
 });
 
